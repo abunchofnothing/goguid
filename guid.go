361618e40874b80e139a1f797a9b527d44b34768
CJ Thompson <cjthomp@gmail.com>
2014-11-07 13:14:08 -0600

    Removed c&p example code

--- guid.go
+++ guid.go
@@ -31,17 +31,3 @@ func Generate() string {
 
 	return fmt.Sprintf("%x-%x-%x-%x-%x", str[0:4], str[4:6], str[6:8], str[8:10], str[10:16])
 }
-
-/*
-	mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
-	$charid = strtolower(md5(uniqid(rand(), true)));
-	$hyphen = chr(45);// "-"
-	$uuid = ""      //chr(123)// "{"
-		.substr($charid, 0, 8).$hyphen
-		.substr($charid, 8, 4).$hyphen
-		.substr($charid,12, 4).$hyphen
-		.substr($charid,16, 4).$hyphen
-		.substr($charid,20,12)
-	;   //.chr(125);// "}"
-	return $uuid;
-*/
